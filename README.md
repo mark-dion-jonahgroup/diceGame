53e026c6920a723a77d64443377fccafe7f71853
Mark Dion <mark.dion@jonahgroup.com>
2022-11-17 15:36:13 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Instructions to install and run the dice-game CLI
+1. From this folder run `npm install -g .`
+2. Run `dice-game` to see the arguments.
+3. Run `dice-game -i 10000 -d 2` to run the simulation 10,000 times with 2 dice.
---
+++ bin/index.js
@@ -0,0 +1,95 @@
+#! /usr/bin/env node
+const yargs = require("yargs");
+
+const usage = "\nUsage: Run simulations for the Dice Game.";
+const options = yargs  
+      .usage(usage)
+      .options({
+        "i": {
+            alias:"iterations",
+            describe: "Number of iterations of the game to run.",
+            type: "int",
+            demandOption: true 
+        },
+        "d": {
+            alias:"dice",
+            describe: "Number of dice to use.",
+            type: "int",
+            demandOption: true 
+        },
+      })
+      .help(true)
+      .argv;
+
+const startTime = Date.now();
+
+const rollDie = () => {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+const rollDice = (numberOfDice) => {
+    let numberOfDiceRemaining = numberOfDice;
+    let containsAThree = false;
+    let lowestDiceRoll = 7;
+
+    let resultsArr = [];
+    for (let i = 0; i < numberOfDice; i++) {
+        const dieValue = rollDie();
+        resultsArr.push(dieValue);
+        if (dieValue < lowestDiceRoll) {
+            lowestDiceRoll = dieValue;
+        }
+        if (dieValue === 3) {
+            containsAThree = true;
+            numberOfDiceRemaining -= 1;
+        }
+    }
+
+    if (containsAThree) {
+        return {
+            score: 0,
+            numberOfDiceRemaining
+        };
+    }
+
+    while (resultsArr.includes(lowestDiceRoll)) {
+        let index = resultsArr.indexOf(lowestDiceRoll);
+        resultsArr.splice(index, 1);
+    }
+
+    return {
+        score: lowestDiceRoll,
+        numberOfDiceRemaining: resultsArr.length
+    };
+}
+
+const playGame = (numberOfDice) => {
+    let score = 0;
+    let remainingDice = numberOfDice;
+    
+    while (remainingDice > 0) {
+        const result = rollDice(remainingDice);
+        score += result.score;
+        remainingDice = result.numberOfDiceRemaining;
+    }
+
+    return score;
+}
+
+console.log(`Number of simulations was ${options.iterations} using ${options.dice} dice.`);
+
+let scores = {};
+
+for (let i = 0; i < options.iterations; i++) {
+    const gameScore = playGame(options.dice);
+    scores[gameScore] = scores[gameScore] ? scores[gameScore] + 1 : 1;
+}
+
+for (const score in scores) {
+    const numberOfOccurrences = scores[score];
+    console.log(`Total ${score} occurs ${numberOfOccurrences / options.iterations} occurred ${numberOfOccurrences} times.`);
+}
+
+const duration = Date.now() - startTime;
+
+console.log(`Total simulation took ${duration / 1000} seconds.`);
